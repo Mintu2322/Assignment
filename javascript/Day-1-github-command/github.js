30d31b5743ce2c2c53d4c71b5c134c59d28b7821
Mintu2322 <mintunagar1023@gmail.com>
2022-08-14 14:42:26 +0530

    add

---
+++ javascript/Day-1-github-command/github.js
@@ -0,0 +1,8 @@
+Hello my name is mintu 
+
+Commands: git clone url - use to clone a repository
+git add - the first command in a chain of operations that directs Git to "save" a snapshot of the current project state, into the commit history. 
+git commit - Commits are the core building block units of a Git project timeline. Commits can be thought of as snapshots or milestones along the timeline of a Git project. Commits are created with the git commit command to capture the state of a project at that point in time. 
+git push - is used to upload local repository content to a remote repository.
+git -- git config --global user.name "Your Name" - to set your Git username
+git config --global user.email "you@example.com" - to set your email
